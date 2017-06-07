d4e4ea2af57671903e29a7ddfd502c349e94ec63
JevgeniD <jevgeni.demidov@pipedrive.com>
2017-06-07 11:55:54 +0300

    fixed issues

--- src/js/fixture-parser.js
+++ src/js/fixture-parser.js
@@ -14,21 +14,10 @@ var FixtureParser = function() {
         return localKOTime;
     };
 
-    var preprocessFixtures = function(fixtures) {
-        $.each(fixtures, function(index, fixture) {
-            if (fixture.country === 'Spain') {
-                if (fixture.kickOff.status === 'FULL_TIME') {
-                    fixture.score.homeGoals = 0;
-                    fixture.score.awayGoals = 0;
-                }
-            }
-        });
-        return fixtures;
-    };
     var getFixtureAsHTMLElement = function(fixture, index) {
         var listElement = '<tr class="fixture">';
         listElement = listElement + '<td class="competition"><div class="flag flag-' + fixture.country + '"></div>' + fixture.competition + '</td>';
-        listElement = listElement + '<td class="kickOffDate" ><small>' + getLocalKickOffTime(fixture.kickOff.date, fixture.kickOff.time) + '</small></td>';
+        listElement = listElement + '<td class="kickOffDate" ><small>' + fixture.kickOff.date + '</small></td>';
         listElement = listElement + '<td class="home team"><strong>' + fixture.awayTeam + '</strong></td>';
         listElement = listElement + '<td class="score">' + fixture.score.homeGoals + ':' + fixture.score.awayGoals + '</td>';
         listElement = listElement + '<td class="away team"><strong>' + fixture.homeTeam + '</strong></td>';
@@ -41,8 +30,7 @@ var FixtureParser = function() {
             var localString = FixtureFinder.localizeString("fixtures");
             $('.fixtures .fixture').remove();
             $('.fixtures .numberOfFixtures').text(fixtures.length + ' ' + localString);
-            fixtures = preprocessFixtures(fixtures);
-            $.each(fixtures.length>=2?fixtures.slice(0, fixtures.length-1):fixtures, function(index, fixture) {
+            $.each(fixtures, function(index, fixture) {
                 $('.fixtures .table').append(getFixtureAsHTMLElement(fixture, index));
             });
 
